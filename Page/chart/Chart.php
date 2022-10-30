928ff6fcda58b23472573b282479b49c1f7da777
Oliver <dragonhatgame@gmail.com>
2022-10-30 16:36:00 +0700

    drawChairFE

---
+++ Page/chart/Chart.php
@@ -0,0 +1,316 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Admin</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+
+    <link href="../../css/bootstrap.min.css" rel="stylesheet">
+    <link href="/DACN/css/chart.css" rel="stylesheet">
+
+    <link href="../../css/style.css" rel="stylesheet">
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <?php
+    echo("<script type='text/javascript'>
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+    function drawChart() {
+
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'Topping');
+      data.addColumn('number', 'Slices');
+      data.addRows([
+        ['Mushrooms', 1],
+        ['Onions', 0],
+        ['Olives', 0],
+        ['Zucchini', 0],
+        ['Pepperoni', 0]
+      ]);
+
+      // Set chart options
+      var options = {'title':'Noi dung1',
+                     'width':500,
+                     'height':500};
+
+      // Instantiate and draw our chart, passing in some options.
+      var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+      chart.draw(data, options);
+    }
+    // bảng 2
+    google.charts.setOnLoadCallback(drawMultSeries);
+
+    function drawMultSeries() {
+        var data = google.visualization.arrayToDataTable([
+            ['Element', 'Density', { role: 'style' } ],
+            ['noidung1', 8.94, '#b87333'],
+            ['noidung2', 10.49, 'silver'],
+            ['noidung3', 19.30, 'gold'],
+            ['noidung4', 21.45, 'color: #e5e4e2']
+        
+          ]);
+    
+          var view = new google.visualization.DataView(data);
+          view.setColumns([0, 1,
+                           { calc: 'stringify',
+                             sourceColumn: 1,
+                             type: 'string',
+                             role: 'annotation' },
+                           2]);
+    
+          var options = {
+            title: 'Bang so2',
+            width: 500,
+            height: 400,
+            bar: {groupWidth: '95%'},
+            legend: { position: 'none' },
+          };
+          var chart = new google.visualization.ColumnChart(document.getElementById('chart1'));
+          chart.draw(view, options);
+    }
+    //bảng 3
+    google.charts.setOnLoadCallback(drawBasic);
+
+    function drawBasic() {
+
+      var data = google.visualization.arrayToDataTable([
+        ['', '2010 Population',],
+        ['Noidung1', 8175000],
+        ['Noidung2', 3792000],
+        ['Noidung3', 2695000],
+        ['Noidung4', 2099000],
+        ['Noidung5', 1526000]
+      ]);
+
+      var options = {
+        title: 'Population of Largest U.S. Cities',
+        chartArea: {width: '50%'},
+        hAxis: {
+          title: 'Total Population',
+          minValue: 0
+        },
+        vAxis: {
+          title: ''
+        }
+      };
+
+      var chart = new google.visualization.BarChart(document.getElementById('chart2'));
+
+      chart.draw(data, options);
+    }
+  </script>")
+    
+    ?>
+</head>
+
+<body>
+    <div class="container-xxl position-relative bg-white d-flex p-0">
+
+        <!-- Sidebar Start -->
+        <div class="sidebar pe-4 pb-3">
+            <nav class="navbar bg-light navbar-light">
+                <a href="#" class="navbar-brand mx-4 mb-3">
+                    <img src="../../img/logohutech.png" alt="" style="width: 200px; height: 45px;">
+                </a>
+                <div class="navbar-nav w-100">
+                <a href="/DACN/homepage/giaovien.php" class="nav-item nav-link"><i class="fa fa-home me-2"></i>Trang Chủ</a>
+                    <a href="/DACN/Page/Lichday/Lichday.php" class="nav-item nav-link"><i class="fa fa-calendar me-2"></i>Lịch dạy</a>
+                    <a href="chart.html" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Biểu đồ</a>
+                    <div class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
+                            <i class="fa fa-bars me-2"></i> Khác</a>
+                        <div class="dropdown-menu bg-transparent border-0">
+                            <a href="/DACN/Login/logoutgiaovien.php" class="dropdown-item"><i class="fa fa-table me-2"></i>Đăng xuất</a>
+                            <a href="/DACN/Login/logoutgiaovien.php" class="dropdown-item"><i class="fa fa-chart-bar me-2"></i>Thoát</a>
+                        </div>
+                    </div>
+                    <a href="/DACN/Page/Support/Support.php" class="nav-item nav-link"><i class="fa fa-phone me-2"></i>Hỗ trợ</a>
+                   
+                </div>
+            </nav>
+        </div>
+        <!-- Sidebar End -->
+        <!-- Content Start -->
+        <div class="content">
+            <!-- Navbar Start -->
+            <nav class="navbar navbar-expand bg-light navbar-light sticky-top px-4 py-0">
+                <a href="index.html" class="navbar-brand d-flex d-lg-none me-4">
+                    <h2 class="text-primary mb-0"></h2>
+                </a>
+                <a href="#" class="sidebar-toggler flex-shrink-0">
+                    <i class="fa fa-bars"></i>
+                </a>
+                <form class="d-none d-md-flex ms-4">
+                    <input class="form-control border-0" type="search" placeholder="Search">
+                </form>
+                <div class="navbar-nav align-items-center ms-auto">
+                    <div class="nav-item dropdown">
+                        
+                    </div>
+                    <div class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
+                            <i class="fa fa-bell me-lg-2"></i>
+                            <span class="d-none d-lg-inline-flex">Thông Báo</span>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
+                        <?php require '../../ConnectDB.php';
+                     $id=$_COOKIE['checkLogin'];
+                     $sql="SELECT * FROM `notification` WHERE `IdUser`=$id ORDER BY `notification`.`id` DESC";
+                     $result=$conn->query($sql);
+                    
+                     while($row = $result->fetch_assoc())
+                     {
+                        
+                     echo('
+                     <a href="'.$row['Link'].'" class="dropdown-item">
+                         <h6 class="fw-normal mb-0">'.$row['content'].'</h6>
+                         <small>15 phút trước</small>
+                     </a>
+                     <hr class="dropdown-divider">
+                 ');
+                     }
+                     ?>
+                        </div>
+                    </div>
+                    <?php
+                     require "../../ConnectDB.php";
+                     if(!empty($_COOKIE['checkLogin']))
+                     {
+                         
+                         $id=$_COOKIE['checkLogin'];
+                         $sql="SELECT * FROM `userInformation` WHERE `idUser`=$id";
+                         $result=$conn->query($sql);
+                         $row=$result->fetch_assoc();
+                         if($row!=null)
+                         {
+                         echo('<div class="nav-item dropdown">
+                         <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
+                             <img class="rounded-circle me-lg-2" src="/DACN/img/'.$row['avatar'].'" alt=""
+                                 style="width: 40px; height: 40px;">
+                             <span class="d-none d-lg-inline-flex">'.$row['nameUser'].'</span>
+                         </a>
+                         <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
+                             <a href="#" class="dropdown-item">Thông tin tài khoản</a>
+                             <a href="#" class="dropdown-item">Cài đặt</a>
+                             <a href="/DACN/Login/logout.php" class="dropdown-item">Đăng xuất</a>
+                         </div>
+                         </div>');
+                         }
+                         else{
+                             echo("<script> var a=confirm('Login False! Please Check Your Input!');
+                                 if(a==true)
+                                 {
+                                     location='../../Login/logout.php';
+                                 }
+                                 else{
+                                     location='../../Login/logout.php';
+                                 }
+                                 </script>");
+                         }
+                    }
+                   else{
+                    echo('<div class="nav-item dropdown"  style="display:flex;">
+                        <a href="" class="nav-link" data-bs-toggle="dropdown">
+                            <img class="rounded-circle me-lg-2" src="/DACN/img/profile-42914_960_720.png" alt=""
+                                style="width: 40px; height: 40px;">
+                            <a href="../Login/login.html" class="d-none d-lg-inline-flex" style="margin-top:20px">Đăng nhập</a>
+                        </a>    
+                    </div>');
+                   }
+                    ?>
+                </div>
+            </nav>
+            <!-- Navbar End -->
+            <div class="container-fluid pt-4 px-4">
+                <div class="row">
+                <?php
+                 require "../../connectDB.php";
+                 $id=$_COOKIE['checkLogin'];
+                 $sql="SELECT nameUser,avatar,`address`,phoneNumber FROM `userInformation` WHERE `idUser`=$id";
+                 $result=$conn->query($sql);
+                 $row = $result->fetch_assoc();
+                    echo('
+                    <div class="circle col-3">
+                        <img src="/DACN/img/'.$row['avatar'].'" alt="">
+                    </div>
+                    <div class="col-9">
+                        <h1 id="nameTeacher">Thầy/Cô : 
+                    ');
+                       
+                        echo($row['nameUser']."</h1>
+                        <hr>
+                        <h5>Số Điện Thoại: ".$row['phoneNumber']."</h5>
+                        <h5>Địa Chỉ: ".$row['address']."</h5>
+
+                        "); 
+                        ?>
+                    </div>
+                </div>
+                <hr>
+                <div class='row'>
+                    <div id="chart_div" class='col-6'></div>
+                    <div class="col-6" id='chart1'></div>
+                    <div class="col-6" id='chart2'></div>
+                </div>
+                   
+            </div>
+
+
+            <!-- Footer Start -->
+            <footer class="bg-lighskyblue text-center text-black">
+            <!-- Grid container -->
+            <div class="container p-4 pb-0">
+            <!-- Section: Social media -->
+            <section class="mb-4">
+                <!-- Facebook -->
+                <a href="https://www.facebook.com/profile.php?id=100010757443088">
+                    <img src="/image/iconSocial1.jpg" width="50" alt=""></a>
+
+                <a href="#">
+                    <img src="/image/iconSocial2.png" width="55" alt=""></a>
+            </section>
+            <!-- Section: Social media -->
+        </div>
+        <!-- Grid container -->
+
+        <!-- Copyright -->
+        <div class="text-center p-3" style="background-color: rgba(204, 229, 255, 0.8);">
+            © 2022 Copyright:
+            <a class="text-black" href="">QTP</a>
+        </div>
+        <div class="text-center p-3" style="background-color: rgba(204, 229, 255, 0.8);">
+            Design by:
+            <a class="text-white" href="">QTP </a>
+            <a>Contact: dragonhatgame@gmail.com</a>
+            , quytrup775@gmail.com, Phat
+        </div>
+
+        <!-- Copyright -->
+            </footer>
+            <!-- Footer End -->
+        </div>
+
+
+
+        <!-- Content End -->
+
+
+        <!-- Back to Top -->
+        <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="../../js/main.js"></script>
+</body>
+
+</html>
